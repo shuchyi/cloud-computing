b2c385c2a64f3e29d6e8c442992bcc5d674fccaf
shuchyi <166594096+shuchyi@users.noreply.github.com>
2024-05-13 14:48:15 +0800

    Update active_msg.php

--- active_msg.php
+++ active_msg.php
@@ -9,9 +9,9 @@
 $msgid = $_GET['msgid'];
 
 // Connect to the database (assuming variables $db_host, $db_user, $db_pass, and $db_name are defined)
-define('DB_HOST', "localhost");
-define('DB_USER', "root");
-define('DB_PASS', "");
+define('DB_HOST', "database6.chqgy0y08gjm.us-east-1.rds.amazonaws.com");
+define('DB_USER', "aws_user");
+define('DB_PASS', "aws_user");
 define('DB_NAME', "assignment1");
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
