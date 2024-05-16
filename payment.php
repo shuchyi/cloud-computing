f357c7fd60c7269ef44d008a1dd97602638de90d
shuchyi <166594096+shuchyi@users.noreply.github.com>
2024-05-16 13:44:53 +0800

    Update payment.php

--- payment.php
+++ payment.php
@@ -1,23 +1,19 @@
-
 <?php
 require_once 'config/db.php';
 
 $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if(isset($_POST["Proceed To Payment"])){
-    // Place your echo statement here
-    echo "<a href='registration.php'>Back to lists </a>";
-
+if (isset($_POST["Proceed To Payment"])) {
     // Proceed with your existing logic for form submission
     // ...
+
+    // Display the success message
+    echo "<script>alert('Payment successful!');</script>";
+    echo "<a href='registration.php'>Back to lists </a>";
 }
 ?>
 
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
--->
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -27,23 +23,14 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
     <link href="css/nicepage.css" rel="stylesheet" type="text/css"/>
     <script src="js/jQuery.js" type="text/javascript"></script>
     <script src="js/nicepage.js" type="text/javascript"></script>
-
-
 </head>
 <body>
-    <?php
-    include 'header.php';
-    ?>
+    <?php include 'header.php'; ?>
     <div class="container">
-
         <form action="" method="POST">
-
             <div class="row">
-
                 <div class="col">
-
                     <h3>billing address</h3>
-
                     <div class="text">
                         <span>full name :</span>
                         <input type="text" placeholder="joel wong" required>
@@ -60,7 +47,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                         <span>city :</span>
                         <input type="text" placeholder="georgetown" required>
                     </div>
-
                     <div class="division">
                         <div class="text">
                             <span>state :</span>
@@ -68,16 +54,12 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                         </div>
                         <div class="text">
                             <span>postcode :</span>
-                            <input type="text" placeholder="123456" max="6" required>
+                            <input type="text" placeholder="123456" maxlength="6" required>
                         </div>
                     </div>
-
                 </div>
-
                 <div class="col">
-
                     <h3>Your Seats</h3>
-
                     <div class="text">
                         <span>cards accepted :</span>
                         <img src="images/card_img.png" alt="">
@@ -94,31 +76,21 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
                         <span>exp month :</span>
                         <input type="text" placeholder="january" required>
                     </div>
-
                     <div class="division">
                         <div class="text">
                             <span>exp year :</span>
-                            <input type="text" placeholder="2022" max="4" required>
+                            <input type="text" placeholder="2022" maxlength="4" required>
                         </div>
                         <div class="text">
                             <span>CVV :</span>
-                            <input type="text" placeholder="1234" max="4" required>
+                            <input type="text" placeholder="1234" maxlength="4" required>
                         </div>
                     </div>
-
                 </div>
-
             </div>
-
-            <button  class="submit" name="Proceed To Payment">Proceed To Payment</button>
-            
-
+            <button class="submit" name="Proceed To Payment">Proceed To Payment</button>
         </form>
     </div>    
-    <?php
-    include_once 'footer.php';
-    ?>
+    <?php include_once 'footer.php'; ?>
 </body>
-
-
 </html>
